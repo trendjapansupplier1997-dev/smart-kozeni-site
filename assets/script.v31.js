1fc972475862a324fc308b12e0a66367c7c861ea
trendjapansupplier1997-dev <trendjapan.supplier1997@gmail.com>
2026-05-07 19:52:10 +0900

    Add files via upload

---
+++ assets/script.v31.js
@@ -0,0 +1,14 @@
+(() => {
+  const links = document.querySelectorAll('[data-referral-link]');
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      const name = link.getAttribute('data-referral-link') || 'referral';
+      if (typeof gtag === 'function') {
+        gtag('event', 'referral_click', { item_name: name, link_url: link.href });
+      }
+      if (typeof clarity === 'function') {
+        clarity('event', 'referral_click_' + name);
+      }
+    });
+  });
+})();
