c8fc2684bfcdda580ccf952523a44e7fa67603fe
hayashi <hayashi.industry1998@gmail.com>
2026-05-08 14:40:37 +0900

    Finalize sitemap XML

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('install', event => { self.skipWaiting(); }); self.addEventListener('activate', event => { event.waitUntil((async () => { const keys = await caches.keys(); await Promise.all(keys.map(key => caches.delete(key))); await self.clients.claim(); })()); });
+/* smart-kozeni service worker v36.10.7: clear old caches and take control */
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.clients.claim();
+  })());
+});
