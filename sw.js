9128d6f06730b6d94d691ff7f8e4fc921bd24e1d
hayashi <hayashi.industry1998@gmail.com>
2026-05-08 15:05:50 +0900

    Unify brand system and simplify entry pages

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-/* smart-kozeni service worker v36.10.9: clear old caches and take control */
+/* smart-kozeni service worker v36.11: clear old caches and take control */
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
   event.waitUntil((async () => {
     const keys = await caches.keys();
