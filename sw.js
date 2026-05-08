48fd9fa3a3ec4b98317f2fde2ec04f3160368ef3
hayashi <hayashi.industry1998@gmail.com>
2026-05-08 14:50:22 +0900

    Simplify homepage to four entry gates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* smart-kozeni service worker v36.10.8: clear old caches and take control */
+/* smart-kozeni service worker v36.10.9: clear old caches and take control */
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
