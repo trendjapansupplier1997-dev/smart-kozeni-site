517c1d36ef7c99bbf51cac4e2d7505de843b553b
hayashi <hayashi.industry1998@gmail.com>
2026-05-08 14:38:13 +0900

    Fix sitemap XML format

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-/* smart-kozeni service worker v36.10.5: clear old caches and take control */
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.map(key => caches.delete(key)));
-    await self.clients.claim();
-  })());
-});
+self.addEventListener('install', event => { self.skipWaiting(); }); self.addEventListener('activate', event => { event.waitUntil((async () => { const keys = await caches.keys(); await Promise.all(keys.map(key => caches.delete(key))); await self.clients.claim(); })()); });
